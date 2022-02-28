c554c872555a2584ec9f829df85d86f4a1b6f115
danishmahmoodawan <dani.ciitwah@gmail.com>
2022-02-28 23:46:18 +0500

    version 1

---
+++ .history/src/pages/Home_20220228190106.js
@@ -0,0 +1,183 @@
+
+import { useState, useEffect } from 'react';
+function Home() {
+    
+  return (
+   <>
+ <section className="top_section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center top_image">
+                        <div className="social_div">
+                            <img src="assets/images/ICON_INSTA24.png" alt="ICON_INSTA24" className="desktop_image"/>
+                            <img src="assets/images/ICON_MEDIUM25.png" alt="ICON_MEDIUM25" className="desktop_image"/>
+                            <img src="assets/images/ICON_OS26.png" alt="ICON_OS26" className="desktop_image"/>
+                            <img src="assets/images/ICON_TWITTER27.png" alt="ICON_TWITTER27" className="desktop_image"/>
+                            <img src="assets/images/ICON_DISCORD28.png" alt="ICON_DISCORD28" className="desktop_image"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="first_section"></section>
+       
+        <section className="second_section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-4 left_sec_col img_col">
+                        <div className="row">
+                          <div className="col-12 right_inner_sec_col row_set_whi">
+                                <div className="row">
+                                    <div className="col-6 col_white">
+                                        <p>PRICE</p>
+                                    </div>
+                                    <div className="col-6 col_black">
+                                        <p>0.15 Ξ</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-6 col_white">
+                                        <p>TOTAL</p>
+                                    </div>
+                                    <div className="col-6 col_black">
+                                        <p>1125 / 3333</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-6 col_white">
+                                        <p>LIMIT</p>
+                                    </div>
+                                    <div className="col-6 col_black">
+                                        <p>5 PER TX</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    <div className="col-12 col-md-2 left_sec_col">
+                        <div className="row"><div className="col-12 icon_size p-0"> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+</svg></div>
+ </div>
+                        <div className="row"><div className="col-12 icon_size p-0"><span class="counts"> 5 </span></div> </div>
+                        <div className="row"><div className="col-12 icon_size p-0"> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+</svg></div>
+ </div>
+                    </div>
+                    <div className="col-12 col-md-2 mintbtns ">
+                    <button classname=" m-2">MINT <br /> NOW</button> 
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section className="third_section">
+            <div className="container-fluid">
+                <div className="row third_row">
+                    <div className="col-md-6 col-12 left_third_col"></div>
+                    <div className="col-md-6 col-12 right_third_col">
+                        <div className="container">
+                            <a className="button rarity">
+                                RARITY
+                            </a>
+                            <div className="row">
+                                <div className="col-6">
+                                    <img src="assets/images/PREVIEW_Artboard.png" alt="PREVIEW_Artboard" className="desktop122_image" />
+                                    <a className="button common">COMMON</a>
+                                </div>
+                                <div className="col-6">
+                                    <img src="assets/images/PREVIEW_Artboard.png" alt="PREVIEW_Artboard" className="desktop122_image" />
+                                    <a className="button uncommon">UNCOMMON</a>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <img src="assets/images/PREVIEW_Artboard.png" alt="PREVIEW_Artboard" className="desktop122_image" />
+                                    <a className="button rare">RARE</a>
+                                </div>
+                                <div className="col-6">
+                                    <img src="assets/images/PREVIEW_Artboard.png" alt="PREVIEW_Artboard" className="desktop122_image" />
+                                    <a className="button epic">EPIC</a>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="forth_section">
+            <div className="container-fluid faq_container">
+                <h2>FREQUENTLY ASKED QUESTIONS</h2>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7 col-12 forth_left_col">
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS HERE SOME QUESTIONS?</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS HERE SOME QUESTIONS?</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS HERE SOME QUESTIONS?</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS</p>
+                        <p>SOME QUESTION NEEDS SOME ANSWERS SOME QUESTIONS HERE SOME QUESTIONS?</p>
+                    </div>
+                    <div className="col-md-5 col-12 forth_right_col">
+                        <img src="assets/images/PREVIEW_Artboard321293.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+                        <p>THE ANSWERS ARE ANSWERED BY THIS KONGZ OBVIOUSLY ANSWER IS ALL HERE!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+     
+        <section className="fifth_section">
+            <div className="container fifth_container">
+                <h2 className="road_map">ROAD MAP</h2>
+                <div className="row">
+                    <div className="col-md-4 col-12 fifth_left_col">
+                        <h2>25%</h2>
+                    </div>
+                    <div className="col-md-8 col-12">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non eros cursus, convallis ligula tristique, malesuada purus. Vestibulum consectetur mollis tristique. Nunc gravida efficitur odio ut luctus. Praesent viverra lorem eu erat hendrerit, eget lacinia elit sagittis. Pellentesque accumsan ac mi eget semper.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 col-12 fifth_left_col">
+                        <h2>50%</h2>
+                    </div>
+                    <div className="col-md-8 col-12">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non eros cursus, convallis ligula tristique, malesuada purus. Vestibulum consectetur mollis tristique. Nunc gravida efficitur odio ut luctus. Praesent viverra lorem eu erat hendrerit, eget lacinia elit sagittis. Pellentesque accumsan ac mi eget semper.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 col-12 fifth_left_col">
+                        <h2>75%</h2>
+                    </div>
+                    <div className="col-md-8 col-12">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non eros cursus, convallis ligula tristique, malesuada purus. Vestibulum consectetur mollis tristique. Nunc gravida efficitur odio ut luctus. Praesent viverra lorem eu erat hendrerit, eget lacinia elit sagittis. Pellentesque accumsan ac mi eget semper.</p>
+                    </div>
+                </div>
+                <div className="row fifth_last_row">
+                    <div className="col-md-4 col-12 fifth_left_col">
+                        <h2>100%</h2>
+                    </div>
+                    <div className="col-md-8 col-12">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non eros cursus, convallis ligula tristique, malesuada purus. Vestibulum consectetur mollis tristique. Nunc gravida efficitur odio ut luctus. Praesent viverra lorem eu erat hendrerit, eget lacinia elit sagittis. Pellentesque accumsan ac mi eget semper.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid logo_container">
+                <div className="row text-center footer_row">
+                    <div className="col-5 col_ew"></div>
+                    <div className="col-2 empty_div">
+                        <img src="assets/images/logo_n.png" alt="LOGO" className="desktop122_image" />
+                    </div>
+                    <div className="col-5"></div>
+                </div>
+            </div>
+        </section>
+	</>
+  );
+}
+
+export default Home;
---
+++ .history/src/pages/Home_20220228222143.js
@@ -0,0 +1,249 @@
+
+import { useState, useEffect } from 'react';
+function Home() {
+    
+  return (
+   <>
+ <section className="top_section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center top_image">
+                        <div className="social_div">
+                            <img src="assets/images/ICON_INSTA24.png" alt="ICON_INSTA24" className="desktop_image"/>
+                            <img src="assets/images/ICON_MEDIUM25.png" alt="ICON_MEDIUM25" className="desktop_image"/>
+                            <img src="assets/images/ICON_OS26.png" alt="ICON_OS26" className="desktop_image"/>
+                            <img src="assets/images/ICON_TWITTER27.png" alt="ICON_TWITTER27" className="desktop_image"/>
+                            <img src="assets/images/ICON_DISCORD28.png" alt="ICON_DISCORD28" className="desktop_image"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="first_section"></section>
+       
+        <section className="second_section">
+            <div className="container">
+                <div className="row d-flex align-items-center justify-content-center">
+                    <div className="col-12 col-md-5 left_sec_col img_col">
+                        <div className="row">
+                          <div className="col-12 right_inner_sec_col row_set_whi">
+                                <div className="row">
+                                    <div className="col-4 col_white">
+                                        <p>PRICE</p>
+                                    </div>
+                                    <div className="col-8 col_black">
+                                        <p>0.15 Ξ</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-4 col_white">
+                                        <p>TOTAL</p>
+                                    </div>
+                                    <div className="col-8 col_black">
+                                        <p>1125 / 3333</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-4 col_white">
+                                        <p>LIMIT</p>
+                                    </div>
+                                    <div className="col-8 col_black">
+                                        <p>5 PER TX</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    <div className="col-12 col-md-2 left_sec_col text-center">
+                        <div className="row"><div className="col-12 icon_size p-0"> <a href="#" target={{target:"_blank"}}> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" className="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+</svg></a></div>
+ </div>
+                        <div className="row"><div className="col-12 icon_size p-0"><span class="counts"> 5 </span></div> </div>
+                        <div className="row"><div className="col-12 icon_size p-0"><a href="#" target={{target:"_blank"}}> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+</svg></a></div>
+ </div>
+                    </div>
+                    <div className="col-12 col-md-2 mintbtns right_col">
+                    <button className=" m-2">MINT <br /> NOW</button> 
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section className="third_section roadmapSec">
+            <div className="container">
+                <div className="row third_row">
+                <div className="col-12 "><h1>ROADMAP</h1></div>                
+                 </div> 
+                 <div className="row set_map">
+                 <div className="col-sm-12 col-md-4">
+                 <div className="btn map_one maptwo btn-primary position-relative">
+             <p>    Lorem ipsum dolor sit amet consectetur adipiscing elit Aliquam ac dapibus orci sit amet laoreet odio Sed commodo nulla sit amet est posuere lobortis Maecenas ultrices justo non cursus finibus </p>
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill set_ict">
+  <img src="assets/images/01.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+  </span>
+  <p className="text-right">25%</p>
+</div>
+                 </div>
+                 <div className="col-sm-12 col-md-4">
+                 <div className="btn map_one maptwo btn-primary position-relative">
+             <p>    Lorem ipsum dolor sit amet consectetur adipiscing elit Aliquam ac dapibus orci sit amet laoreet odio Sed commodo nulla sit amet est posuere lobortis Maecenas ultrices justo non cursus finibus </p>
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill set_ict">
+  <img src="assets/images/02.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+  </span>
+  <p className="text-right">25%</p>
+</div>
+                 </div>
+                 <div className="col-sm-12 col-md-4">
+                 <div className="btn map_one maptwo btn-primary position-relative">
+             <p>    Lorem ipsum dolor sit amet consectetur adipiscing elit Aliquam ac dapibus orci sit amet laoreet odio Sed commodo nulla sit amet est posuere lobortis Maecenas ultrices justo non cursus finibus </p>
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill set_ict">
+  <img src="assets/images/03.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+  </span>
+  <p className="text-right">25%</p>
+</div>
+                 </div>
+                 </div>
+             </div>
+        </section>
+        <section className="forth_section faq_container">
+            <div className="container">
+            <div className="row">
+            <div className="col-4 faq_col p-5">
+            <img src="assets/images/faq.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+</div>
+            <div className="col-8 p-5">
+            <img src="assets/images/answer.png" alt="PREVIEW_Artboard321293" className="desktop122_image" />
+            </div>
+            </div>
+           
+              
+                        
+<div className="row">
+<div className="col-md-4 bg_link faq_tabss p-0">
+
+    <ul className="nav md-pills pills-secondary d-flex flex-column">
+      <li className="nav-item">
+        <a className="nav-link active" data-toggle="tab" href="#panel11" role="tab">These are some questions to ask for questions to ask</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" data-toggle="tab" href="#panel12" role="tab">These are some questions to ask for questions to ask</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" data-toggle="tab" href="#panel13" role="tab">These are some questions to ask for questions to ask</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" data-toggle="tab" href="#panel14" role="tab">These are some questions to ask for questions to ask</a>
+      </li>
+    </ul>
+
+  </div>
+  <div className="col-md-8 logo_ns">
+
+    <div className="tab-content pt-0">
+
+      <div className="tab-pane fade in show active" id="panel11" role="tabpanel">
+        <br />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+          <img src="assets/images/logo_set.png" alt="LOGO" className="desktop122_image" />
+
+
+      </div>
+      <div className="tab-pane fade" id="panel12" role="tabpanel">
+        <br />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+
+      </div>
+      <div className="tab-pane fade" id="panel13" role="tabpanel">
+        <br />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+          <img src="assets/images/logo_set.png" alt="LOGO" className="desktop122_image" />
+
+
+      </div>
+      <div className="tab-pane fade" id="panel14" role="tabpanel">
+        <br />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+          reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+          porro voluptate odit minima.</p>
+
+      </div>
+     
+
+    </div>
+
+
+</div>
+
+</div>
+</div>
+
+                
+        </section>
+     
+        <section className="fifth_section">
+            <div className="container-fluid fifth_container">
+            <div className="row"><div className="col-12">
+                <h2 className="road_map">SNEAKPEEK</h2>
+            </div></div></div>
+        </section>
+        <section className="six_section">
+            <div className="container-fluid">
+            <div className="row">
+            <div className="col-4">
+            <img src="assets/images/char_img.png" alt="char_img" className="desktop122_image" />
+            </div>
+            <div className="col-4">
+            <img src="assets/images/char_img.png" alt="char_img" className="desktop122_image" />
+            </div>
+            <div className="col-4">
+            <img src="assets/images/char_img.png" alt="char_img" className="desktop122_image" />
+            </div>
+            
+            </div>
+            </div>
+        </section>
+        <section>
+        <div className="row">
+            <div className="col-12">
+            <div className="empty_div"> </div>
+            </div>
+            <div className="col-12">
+            <div className="empty_divs"> </div>
+            </div>
+            </div>
+        </section>
+        <section className="section_seven">
+        <div className="container">
+        <div className="row">
+            <div className="col-4 col_white ">
+            <img src="assets/images/project_name.png" alt="char_img" className="desktop122_image" />
+            </div>
+            </div>
+            </div>
+        </section>
+        
+	</>
+  );
+}
+
+export default Home;
